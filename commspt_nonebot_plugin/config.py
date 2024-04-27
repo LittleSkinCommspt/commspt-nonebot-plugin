54cfd3d5797c55294588675fb864bc842e156b8f
XieXiLin <support@xiexilin.com>
2024-04-27 09:05:08 +0800

    🛠️ fix: add config

--- commspt_nonebot_plugin/config.py
+++ commspt_nonebot_plugin/config.py
@@ -1,12 +1,13 @@
 from nonebot import get_plugin_config
 from pydantic import BaseModel
-
+from typing import List
 
 class ConfigModel(BaseModel):
     ltsk_api_token: str
     ltsk_browserless_api: str
     ltsk_ipip_api: str
     ltsk_skinrendermc_api: str
+    ltsk_commspt_group: List[str]
 
 
 config: ConfigModel = get_plugin_config(ConfigModel)
